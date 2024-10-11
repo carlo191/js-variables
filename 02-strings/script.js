eec08fc73844fc731325643f33056e4427456399
Carlo Elvisi <carloelvisi01@outlook.it>
2024-10-11 15:38:58 +0200

    cartella 2 es 2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,7 +8,10 @@ console.log(name)
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+let number= 9
+let text = 'ok'
+let bool=true
+console.log(number, text, bool)
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
